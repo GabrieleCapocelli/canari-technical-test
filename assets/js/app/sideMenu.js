785ed3108500a36ea1952107c8a8a4b18e51e112
nicolas <nicolas@canari.studio>
2022-02-10 17:12:17 +0100

    ecodechets start front

---
+++ assets/js/app/sideMenu.js
@@ -0,0 +1,12 @@
+(function($, window, undefined) {
+
+    var $navBar = $('#navigation');
+
+    $navBar.hover(function(){
+        $(this).toggleClass('is-open');
+    });
+    console.log('trest');
+
+})(jQuery, window);
+
+
